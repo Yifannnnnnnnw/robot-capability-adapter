4353870260568f45fe6fec97f5dffd195a44449c
Yifan Wang <yifanwang.career@gmail.com>
2026-08-20 16:33:36 +0100

    Add LEAP hand joint-position skeleton

---
+++ autoadapter/libraries/robots/leap_hand/1.0.0/skeleton/hand_joint_position.py
@@ -0,0 +1,8 @@
+"""Public hand skeleton inventory for the canonical right LEAP Hand."""
+
+from autoadapter2.trusted_skeletons.hand_joint_position import (
+    HandJointPositionSkeleton,
+    HandJointPositionSpec,
+)
+
+__all__ = ["HandJointPositionSkeleton", "HandJointPositionSpec"]
--- autoadapter/src/autoadapter2/trusted_skeletons/__init__.py
+++ autoadapter/src/autoadapter2/trusted_skeletons/__init__.py
@@ -1,6 +1,7 @@
 """Trusted, capability-neutral skeletons owned by the mainline Framework."""
 
 from .arm_serial_dls import ArmSerialDLSSkeleton, ArmSpec, IKUnreachableError
+from .hand_joint_position import HandJointPositionSkeleton, HandJointPositionSpec
 from .quadruped_pd_gait import QuadrupedPDGaitSkeleton, QuadrupedSpec
 from .quadruped_position_policy import (
     BarkourPositionPolicySkeleton,
@@ -12,6 +13,8 @@ __all__ = [
     "ArmSpec",
     "BarkourPositionPolicySkeleton",
     "IKUnreachableError",
+    "HandJointPositionSkeleton",
+    "HandJointPositionSpec",
     "QuadrupedPDGaitSkeleton",
     "QuadrupedPositionPolicySpec",
     "QuadrupedSpec",
